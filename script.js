428f6d695fee0563384b790d6386af7186b092ce
Henrique Nascimento <hsncorretor@gmail.com>
2021-12-10 11:30:30 -0300

    requisito 6 ok

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const board = document.getElementById('pixel-board');
 
 function criandoGrade(quadro) {
-
   for (let index = 1; index <= quadro; index += 1) {
     const linha = document.createElement('div');
     linha.className = 'pixel';
     board.appendChild(linha);
 
-    for (let index2 = 1; index2 <= quadro; index2 += 1) {
-      const pixel = document.createElement('div');
-      linha.appendChild(pixel);
-    }
+    const pixel = document.createElement('div');
+    linha.appendChild(pixel);
   }
 }
 criandoGrade(25);
